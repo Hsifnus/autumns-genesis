1a152a40d6047dcd49006f54b7b1085210658f0b
melonfishy <bakafish@berkeley.edu>
2020-03-16 03:20:57 -0700

    add missing files woops

---
+++ js/game/feature/player/coquelicot-player.js
@@ -0,0 +1,29 @@
+ig.module("game.feature.player.coquelicot-player").requires(
+    "game.feature.player.entities.player",
+    "game.feature.player.player-config").defines(function() {
+    sc.PLAYER_CLASSES.SPIRIT = 6;
+    ig.ENTITY.Player.inject({
+        initIdleActions: function() {
+            this.parent();
+            if (this.model.name == "Coquelicot") {
+                var a = new ig.Action("PLAYER_IDLE", [{
+                        type: "SET_FACE",
+                        face: "SOUTH",
+                        rotate: true
+                    }, {
+                        type: "SHOW_ANIMATION",
+                        anim: "idleBob",
+                        wait: false
+                    }, {
+                        time: 6,
+                        type: "WAIT"
+                    }, {
+                        type: "SHOW_ANIMATION",
+                        anim: "preIdle",
+                        wait: true
+                    }], false, false);
+                this.idle.actions.push(a)
+            }
+        }
+    });
+});
