085b3952c8390a08c685688a2b8800cc089f4f20
melonfishy <bakafish@berkeley.edu>
2020-03-24 21:42:48 -0700

    fix fancy aim bug, decrease icicles in shield circle

--- js/game/feature/combat/fancy-aim-fix.js
+++ js/game/feature/combat/fancy-aim-fix.js
@@ -2,6 +2,9 @@ ig.module("game.feature.combat.fancy-aim-fix").requires(
     "game.feature.combat.combat-action-steps"
 ).defines(function() {
     ig.ACTION_STEP.FANCY_AIM.inject({
+        start: function(a) {
+            a.getTarget() && this.parent(a);
+        },
         run: function(a) {
             var b = a.stepData.throwDir,
                 c = a.stepData.sidePos,
