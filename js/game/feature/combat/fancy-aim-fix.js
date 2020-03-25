9c2933bf183bc5ad2740f045644466d41687c8d0
Node Code Formatter <formatter@1337z.ninja>
2020-03-25 00:37:21 +0000

    Formatted code

--- js/game/feature/combat/fancy-aim-fix.js
+++ js/game/feature/combat/fancy-aim-fix.js
@@ -3,9 +3,9 @@ ig.module("game.feature.combat.fancy-aim-fix").requires(
 ).defines(function() {
     ig.ACTION_STEP.FANCY_AIM.inject({
         run: function(a) {
-            var b = a.stepData.throwDir
-              , c = a.stepData.sidePos
-              , d = a.getTarget();
+            var b = a.stepData.throwDir,
+                c = a.stepData.sidePos,
+                d = a.getTarget();
             return !d || this.parent(a);
         }
     });
