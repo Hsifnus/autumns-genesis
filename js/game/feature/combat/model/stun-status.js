2080f3144b8866ebeac95eca7d79f3ea03e099d2
melonfishy <bakafish@berkeley.edu>
2020-03-26 09:28:29 -0700

    delegate custom modifier work to modifier-api

--- js/game/feature/combat/model/stun-status.js
+++ js/game/feature/combat/model/stun-status.js
@@ -1,8 +1,7 @@
 ig.module("game.feature.combat.model.stun-status").requires(
         "game.feature.combat.model.combat-status",
         "game.feature.combat.gui.status-bar",
-        "game.feature.combat.entities.combatant",
-        "game.feature.combat.model.combat-params")
+        "game.feature.combat.entities.combatant")
     .defines(function() {
         sc.StunStatus = sc.COMBAT_STATUS[4] = sc.CombatStatusBase.extend({
             id: 4,
@@ -123,6 +122,7 @@ ig.module("game.feature.combat.model.stun-status").requires(
                 h != 1 && b.undoTransform()
             }
         });
+
         var b = Vec2.create(),
             a = Vec2.create(),
             d = Vec3.create(),
@@ -283,106 +283,15 @@ ig.module("game.feature.combat.model.stun-status").requires(
                 return true
             }
         });
-        var aConst = 0.25,
-            dConst = 1.5,
-            cConst = 3;
-        var funcs = {
-            LINEAR: function(a, b) {
-                return a * 2 - b
-            },
-            PERCENTAGE: function(a, b) {
-                return a > b ? a * (1 + Math.pow(1 - b / a, 0.5) * 0.2) : a * Math.pow(a / b, 1.5)
-            }
+
+        !sc.DAMAGE_MODIFIER_FUNCS && (sc.DAMAGE_MODIFIER_FUNCS = {});
+        sc.DAMAGE_MODIFIER_FUNCS.WIND_MELEE = (attackInfo, damageFactor, combatantRoot, shieldResult, hitIgnore, params) => {
+            var n, applyDamageCallback;
+            !attackInfo.element && attackInfo.skillBonus == "MELEE_DMG"
+                && (n = attackInfo.attackerParams.getModifier("WIND_MELEE")) 
+                && (attackInfo.statusInflict = attackInfo.statusInflict + 0.75)
+                && (damageFactor = damageFactor + damageFactor * n);
+            return { attackInfo, damageFactor, applyDamageCallback };
         };
-        sc.CombatParams.inject({
-            init: function(a) {
-                if (a)
-                    for (var b in this.baseParams) this.baseParams[b] = a[b] || this.baseParams[b];
-                this.currentHp = this.getStat("hp");
-                for (b = 0; b < 5; ++b) this.statusStates[b] = new sc.COMBAT_STATUS[b]
-            },
-            getDamage: function(e, g, h, i, j) {
-                var k = e.damageFactor,
-                    l = e.noHack || false,
-                    o = h.getCombatantRoot(),
-                    h = h.combo || o.combo;
-                if (h.damageCeiling) {
-                    var m = Math.max(1 - (h.damageCeiling.sum[this.combatant.id] || 0) / h.damageCeiling.max, 0);
-                    m < 0.5 && (k = Math.max(k * 2 * m, 0.01))
-                }
-                h = k;
-                if (!ig.perf.skipDmgModifiers) {
-                    e.skillBonus && (k = k * (1 + e.attackerParams.getModifier(e.skillBonus)));
-                    var n = e.attackerParams.getModifier("BERSERK");
-                    n && e.attackerParams.getHpFactor() <= sc.HP_LOW_WARNING && (k = k * (1 + n));
-                    (n = e.attackerParams.getModifier("MOMENTUM")) && (o.isPlayer && o.dashAttackCount) && (k = k * (1 + o.dashAttackCount * n));
-                    !e.element && (n = e.attackerParams.getModifier("WIND_MELEE")) && e.skillBonus == "MELEE_DMG" && (e.statusInflict = e.statusInflict + 3 * n) && (k = k + n * k);
-                    !ig.vars.get("g.newgame.ignoreSergeyHax") &&
-                        (o.isPlayer && !this.combatant.isPlayer && sc.newgame.get("sergey-hax")) && (k = k * 4096);
-                }
-                var g = this.damageFactor * (g === void 0 ? 1 : g),
-                    p = 1,
-                    r = e.attackerParams.getStat("focus", l) / this.getStat("focus", l),
-                    n = (Math.pow(r, 0.35) - 0.9) * e.critFactor,
-                    n = Math.random() <= n;
-                if (!ig.perf.skipDmgModifiers) {
-                    e.element && (p = this.getStat("elemFactor")[e.element - 1] * this.tmpElemFactor[e.element - 1]);
-                    g = g * p;
-                    e.ballDamage && (g = g * (this.ballFactor + this.statusStates[3].getValue(this)));
-                    (m = e.attackerParams.getModifier("CROSS_COUNTER")) && sc.EnemyAnno.isCrossCounterEffective(this.combatant) &&
-                        (g = g * (1 + m));
-                    (m = e.attackerParams.getModifier("BREAK_DMG")) && sc.EnemyAnno.isWeak(this.combatant) && (g = g * (1 + m));
-                    n && (k = k * e.attackerParams.criticalDmgFactor)
-                }
-                o = sc.combat.getGlobalDmgFactor(o.party);
-                m = 0;
-                if (e.statusInflict && g > 0 && !j)
-                    var idx = e.element - 1,
-                        m = h * e.statusInflict;
-                var v = (Math.pow(1 + (r >= 1 ? r - 1 : 1 - r) * cConst, aConst) - 1) * dConst;
-                r = r >= 1 ? 1 + v : Math.max(0, 1 - v);
-                if (idx >= 0) {
-                    m = m * r * this.getStat("statusInflict")[idx] * this.tmpStatusInflict[idx] * p;
-                    m = this.statusStates[idx].getInflictValue(m, this, e, i);
-                } else {
-                    m = m * r * p;
-                    m = this.statusStates[4].getInflictValue(m, this, e, i);
-                }
-                i = e.attackerParams.getStat("attack", l);
-                l = e.defenseFactor *
-                    this.getStat("defense", l);
-                l = Math.max(1, funcs.PERCENTAGE(i, l));
-                l = l * g;
-                i = funcs.PERCENTAGE(i, 0) - l;
-                l = l * k * o;
-                i = i * k * o;
-                if (!ig.perf.skipDmgModifiers) {
-                    l = l * (0.95 + Math.random() * 0.1);
-                    i = i * (0.95 + Math.random() * 0.1)
-                }
-                if (e.limiter.noDmg) i = l = 0;
-                l = Math.round(l);
-                return {
-                    damage: l,
-                    defReduced: i,
-                    offensiveFactor: k,
-                    baseOffensiveFactor: h,
-                    defensiveFactor: g,
-                    critical: n,
-                    status: m
-                }
-            },
-            applyDamage: function(a, b, c) {
-                var d = c.getCombatantRoot(),
-                    c = c.combo || d.combo;
-                if (c.damageCeiling) {
-                    d = this.combatant.id;
-                    c.damageCeiling.sum[d] || (c.damageCeiling.sum[d] =
-                        0);
-                    c.damageCeiling.sum[d] = c.damageCeiling.sum[d] + a.baseOffensiveFactor
-                }
-                a.status && this.statusStates[!!b.element ? b.element - 1 : 4].inflict(a.status, this, b);
-                this.reduceHp(a.damage)
-            }
-        });
+
     });
