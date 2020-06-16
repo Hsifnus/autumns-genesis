960a4a5b02d19eb45ec42e22f0502b7143afd854
Hsifnus <bakafish@Js-MacBook-Pro.local>
2020-06-16 00:37:39 -0700

    don't forget new script too

---
+++ js/game/feature/combat/model/enemy-tracker-access.js
@@ -0,0 +1,42 @@
+ig.module("game.feature.combat.model.enemy-tracker-access")
+.requires("game.feature.combat.model.enemy-tracker", "game.feature.combat.entities.enemy")
+.defines(function() {
+	sc.EnemyTracker.inject({
+		onVarAccess: function(b, a) {}
+	});
+	sc.ENEMY_TRACKER.TIME.inject({
+		OnVarAccess: function(b, a) {
+			if (a[3] == "progress") return this.target ? (this.current / this.target) : 0;
+		},
+	});
+	sc.ENEMY_TRACKER.HIT.inject({
+		target: 0,
+		onConditionEval: function(b, a, d, c) {
+			this,target = this._getTarget(b);
+			return this.parent(b, a, d, c);
+		},
+		onVarAccess: function(b, a) {
+			var t = this.target;
+			if (a[3] == "progress") return t ? (this.current / t) : 0;
+			throw Error("Unsupported var access path: " + b);
+		}
+	});
+	sc.ENEMY_TRACKER.HP.inject({
+		progress: 0,
+		onConditionEval: function(b, a, d, c) {
+			var result = this.parent(b, a, d, c);
+			this.progress = this.hpReduced / b.params.getStat("hp") / this.target;
+			return result;
+		},
+		onVarAccess: function(b, a) {
+			if (a[3] == "progress") return this.progress;
+			throw Error("Unsupported var access path: " + b);
+		}
+	});
+	ig.ENTITY.Enemy.inject({
+		onVarAccess: function(a, b) {
+			console.log(a, b);
+            return b[1] == "tracker" ? this.trackers[b[2]].onVarAccess(a, b) : this.parent(a, b);
+        }
+	})
+});
