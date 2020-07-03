c50493e5b94f32d8ebea3fd15e70adef5f07f16b
Node Code Formatter <formatter@1337z.ninja>
2020-07-03 17:03:27 +0000

    Formatted code

--- js/game/feature/combat/set-attrib-closest-entity-fix.js
+++ js/game/feature/combat/set-attrib-closest-entity-fix.js
@@ -1,8 +1,8 @@
 ig.module("game.feature.combat.set-attrib-closest-entity-fix").requires("game.feature.combat.combat-action-steps").defines(function() {
-	ig.ACTION_STEP.SET_ATTRIB_CLOSEST_ENTITY.inject({
-		start: function(a) {
-			!a.getTarget() && (this.selectBy = 2);
-			this.parent(a);
-		}
-	});
+    ig.ACTION_STEP.SET_ATTRIB_CLOSEST_ENTITY.inject({
+        start: function(a) {
+            !a.getTarget() && (this.selectBy = 2);
+            this.parent(a);
+        }
+    });
 });
