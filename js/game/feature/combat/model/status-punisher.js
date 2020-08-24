49eff8fa3d1998420826626ff8654b09a5ca4ab9
Node Code Formatter <formatter@1337z.ninja>
2020-08-24 07:47:21 +0000

    Formatted code

--- js/game/feature/combat/model/status-punisher.js
+++ js/game/feature/combat/model/status-punisher.js
@@ -3,10 +3,11 @@ ig.module("game.feature.combat.model.status-punisher").requires(
         "game.feature.combat.model.modifier-apply")
     .defines(function() {
         sc.DAMAGE_MODIFIER_FUNCS.STATUS_PUNISHER = (attackInfo, damageFactor, combatantRoot, shieldResult, hitIgnore, params) => {
-            var n = attackInfo.attackerParams.getModifier("STATUS_PUNISHER"), applyDamageCallback,
-            attackInfo = {
-                ...attackInfo
-            };
+            var n = attackInfo.attackerParams.getModifier("STATUS_PUNISHER"),
+                applyDamageCallback,
+                attackInfo = {
+                    ...attackInfo
+                };
             var idx = attackInfo.element ? attackInfo.element - 1 : 4;
             if (params.statusStates[idx].active) {
                 damageFactor = damageFactor + damageFactor * n
